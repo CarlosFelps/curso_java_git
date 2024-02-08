0c9a5edbc2431e6d978269b76812ea9759f5e1d1
Carlos Felipe <carlosfelipe037@gmail.com>
2024-02-08 00:08:10 -0300

    Criação curso java

---
+++ src/RealizandoSoma.java
@@ -0,0 +1,10 @@
+public class RealizandoSoma {
+    public static void main(String[] args) {
+        int anoNascimento = 1994;
+        int anoAtual = 2024;
+        
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println(idade);
+    }
+}
